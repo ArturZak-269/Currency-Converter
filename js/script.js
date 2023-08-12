1caa04e0fa61ae61a8ecd28b03fce7de8ab3feda
Artur Żak <artur.zak.pl@gmail.com>
2023-08-12 13:52:04 +0200

    Updated exchange rates + prevent negative/zero

--- js/script.js
+++ js/script.js
@@ -4,19 +4,24 @@ const currencies = {
   EUR: "Euro",
   TRY: "Turecka Lira",
   USD: "Dolar Amerykański",
- };
- const currencies2 = {
+};
+
+const currencies2 = {
   USD: "Dolar Amerykański",
   TRY: "Turecka Lira",
   EUR: "Euro",
   GBP: "Brytyjski Funt",
   PLN: "Polski złoty",
- };
+};
+
+let exchangeRates = {};
 
 const primaryCurrency = document.getElementById("primary");
 const secondaryCurrency = document.getElementById("secondary");
 primaryCurrency.innerHTML = getOptions(currencies);
 secondaryCurrency.innerHTML = getOptions(currencies2);
+
+primaryCurrency.addEventListener("change", fetchCurrencies);
 
 function getOptions(data) {
   return Object.entries(data)
@@ -24,32 +29,43 @@ function getOptions(data) {
     .join("");
 }
 
-
-
-document.getElementById("btn-convert").addEventListener("click", fetchCurrencies);
-
 function fetchCurrencies() {
   const primary = primaryCurrency.value;
   const secondary = secondaryCurrency.value;
-  const amount = document.getElementById("amount").value;
-  // Important: Include your API key below
-  fetch("https://v6.exchangerate-api.com/v6/084cf46afd18beaf45e76f4d/latest/" + primary)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("NETWORK RESPONSE ERROR");
-      }
-    })
-    .then((data) => {
-      console.log(data);
-      displayCurrency(data, primary, secondary, amount);
-    })
-    .catch((error) => console.error("FETCH ERROR:", error));
+
+  if (exchangeRates[primary]) {
+    displayCurrency(exchangeRates[primary], primary, secondary);
+  } else {
+    fetch("https://v6.exchangerate-api.com/v6/084cf46afd18beaf45e76f4d/latest/" + primary)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("NETWORK RESPONSE ERROR");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        exchangeRates[primary] = data;
+        displayCurrency(data, primary, secondary);
+      })
+      .catch((error) => console.error("FETCH ERROR:", error));
+  }
 }
-function displayCurrency(data, primary, secondary, amount) {
+
+function displayCurrency(data, primary, secondary) {
+  const amount = parseFloat(document.getElementById("amount").value);
+
+  if (isNaN(amount) || amount <= 0) {
+    // Show error message
+    alert("Please enter a valid positive amount.");
+    return;
+  }
+
   const calculated = amount * data.conversion_rates[secondary];
-  document.getElementById("result").setAttribute("style", "display:block");
+  document.getElementById("result").style.display = "block";
   document.getElementById("txt-primary").innerText = amount + " " + primary + " = ";
   document.getElementById("txt-secondary").innerText = calculated.toFixed(2) + " " + secondary;
 }
+
+document.getElementById("btn-convert").addEventListener("click", fetchCurrencies);
